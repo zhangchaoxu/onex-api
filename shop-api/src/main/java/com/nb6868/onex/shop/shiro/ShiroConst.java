c2ed73a302ecbc48295f333e174dc30bebf097a8
Charles <zhangchaoxu@gmail.com>
2021-09-23 10:44:27 +0800

    fix shiro issue

---
+++ shop-api/src/main/java/com/nb6868/onex/shop/shiro/ShiroConst.java
@@ -0,0 +1,20 @@
+package com.nb6868.onex.shop.shiro;
+
+/**
+ * 常量
+ */
+public interface ShiroConst {
+
+    // 用户角色关系表
+    String TABLE_USER_ROLE = "uc_role_user";
+
+    // 用户表
+    String TABLE_USER = "uc_user";
+
+    // token表
+    String TABLE_TOKEN = "uc_token";
+
+    // 权限关系表
+    String TABLE_MENU_SCOPE = "uc_menu_scope";
+
+}
--- shop-api/src/main/java/com/nb6868/onex/shop/shiro/ShiroDao.java
+++ shop-api/src/main/java/com/nb6868/onex/shop/shiro/ShiroDao.java
@@ -1,6 +1,5 @@
 package com.nb6868.onex.shop.shiro;
 
-import com.nb6868.onex.shop.modules.uc.UcConst;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
@@ -20,30 +19,31 @@ public interface ShiroDao {
     /**
      * 获得用户角色列表
      */
-    @Select("select user_id from " + UcConst.TABLE_USER_ROLE + " where deleted = 0 and user_id = #{userId}")
+    @Select("select user_id from " + ShiroConst.TABLE_USER_ROLE + " where deleted = 0 and user_id = #{userId}")
     List<Long> getUserRolesByUserId(@Param("userId") Long userId);
 
     /**
      * 获得用户角色列表
      */
-    @Select("SELECT" +
-            " uc_menu_scope.menu_permissions AS permissions FROM uc_menu_scope" +
-            " WHERE uc_menu_scope.deleted = 0 AND uc_menu_scope.menu_permissions != ''" +
-            " AND ((uc_menu_scope.type = 1  AND uc_menu_scope.role_id IN ( SELECT role_id FROM uc_role_user WHERE uc_role_user.deleted = 0 AND uc_role_user.user_id = #{userId})) OR " +
-            "(uc_menu_scope.type = 2 AND uc_menu_scope.user_id = #{userId}))" +
-            " GROUP BY uc_menu_scope.menu_id")
+    @Select("SELECT " +
+            ShiroConst.TABLE_MENU_SCOPE + ".menu_permissions AS permissions FROM " + ShiroConst.TABLE_MENU_SCOPE +
+            " WHERE " + ShiroConst.TABLE_MENU_SCOPE + ".deleted = 0 AND " + ShiroConst.TABLE_MENU_SCOPE + ".menu_permissions != ''" +
+            " AND (("+ ShiroConst.TABLE_MENU_SCOPE +".type = 1  AND " + ShiroConst.TABLE_MENU_SCOPE + ".role_id IN " +
+            "( SELECT role_id FROM " + ShiroConst.TABLE_USER_ROLE + " WHERE " + ShiroConst.TABLE_USER_ROLE + ".deleted = 0 AND " + ShiroConst.TABLE_USER_ROLE + ".user_id = #{userId})) OR " +
+            "(" + ShiroConst.TABLE_MENU_SCOPE + ".type = 2 AND " + ShiroConst.TABLE_MENU_SCOPE + ".user_id = #{userId}))" +
+            " GROUP BY " + ShiroConst.TABLE_MENU_SCOPE + ".menu_id")
     List<String> getPermissionsListByUserId(@Param("userId") Long userId);
 
     /**
      * 通过id获得用户
      */
-    @Select("select * from " + UcConst.TABLE_USER + " where deleted = 0 and id = #{id} limit 1")
+    @Select("select * from " + ShiroConst.TABLE_USER + " where deleted = 0 and id = #{id} limit 1")
     Map<String, Object> getUserById(@Param("id") Long id);
 
-    @Select("select type, user_id from " + UcConst.TABLE_TOKEN + " where deleted = 0 and token = #{token} and expire_time > now() limit 1")
+    @Select("select type, user_id from " + ShiroConst.TABLE_TOKEN + " where deleted = 0 and token = #{token} and expire_time > now() limit 1")
     Map<String, Object> getUserTokenByToken(@Param("token") String token);
 
-    @Update("update " + UcConst.TABLE_TOKEN + " set expire_time = DATE_ADD(NOW(), interval #{expireTime} second) where deleted = 0 and token = #{token}")
+    @Update("update " + ShiroConst.TABLE_TOKEN + " set expire_time = DATE_ADD(NOW(), interval #{expireTime} second) where deleted = 0 and token = #{token}")
     boolean updateTokenExpireTime(@Param("token") String token, @Param("expireTime") Integer expireTime);
 
 }
