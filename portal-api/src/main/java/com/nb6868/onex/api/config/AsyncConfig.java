619c7dd0dc06e0f552cf62c60b92f99d1e963f0f
Charles <zhangchaoxu@gmail.com>
2021-10-08 17:47:15 +0800

    add async task test

--- portal-api/src/main/java/com/nb6868/onex/api/config/AsyncConfig.java
+++ portal-api/src/main/java/com/nb6868/onex/api/config/AsyncConfig.java
@@ -46,8 +46,8 @@ public class AsyncConfig implements AsyncConfigurer {
          * 当线程池的任务缓存队列已满，并且线程池中的线程数目达到maximumPoolSize,如果还有任务到来就会采取任务拒绝策略
          * 通常有以下四种策略:
          * ThreadPoolExecutor.AbortPolicy:丢弃任务并抛出RejectedExecutionException异常。
-         * ThreadPoolExecutor.DiscardPolicy: 也是丢弃任务，但是不抛出异常。
-         * ThreadPoolExecutor.DiscardOldestPolicy: 丢弃队列最前面的任务，然后重新尝试执行任务(重复此过程)
+         * ThreadPoolExecutor.DiscardPolicy:也是丢弃任务，但是不抛出异常。
+         * ThreadPoolExecutor.DiscardOldestPolicy:丢弃队列最前面的任务，然后重新尝试执行任务(重复此过程)
          * ThreadPoolExecutor.CallerRunsPolicy:重试添加当前的任务，自动重复调用execute()方法，直到成功
          */
         // 拒绝策略
