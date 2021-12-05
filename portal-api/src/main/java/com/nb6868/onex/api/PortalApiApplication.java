54806c375ccbbae43b8398e346c4f1a738f59e70
Charles <zhangchaoxu@gmail.com>
2021-12-05 12:41:49 +0800

    upgrade libs

--- portal-api/src/main/java/com/nb6868/onex/api/PortalApiApplication.java
+++ portal-api/src/main/java/com/nb6868/onex/api/PortalApiApplication.java
@@ -1,42 +1,24 @@
 package com.nb6868.onex.api;
 
+import com.nb6868.onex.common.BaseApplication;
 import com.nb6868.onex.common.util.SpringBeanNameGenerator;
 import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.core.env.Environment;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@MapperScan(nameGenerator = SpringBeanNameGenerator.class, basePackages = {"com.nb6868.onex.**.dao"})
 @ComponentScan(nameGenerator = SpringBeanNameGenerator.class, basePackages = {"com.nb6868.onex.**"})
-@EnableAsync
 @Slf4j
-public class PortalApiApplication extends SpringBootServletInitializer {
+public class PortalApiApplication extends BaseApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext app = SpringApplication.run(PortalApiApplication.class, args);
-
-        Environment env = app.getEnvironment();
-        log.info("\n################## {} Running ##################\n" +
-                        ":: Link ::\thttp://127.0.0.1:{}{}\n" +
-                        ":: Doc ::\thttp://127.0.0.1:{}{}/doc.html\n" +
-                        "################## {} Running ##################",
-                env.getProperty("spring.application.name"),
-                env.getProperty("server.port"),
-                env.getProperty("server.servlet.context-path"),
-                env.getProperty("server.port"),
-                env.getProperty("server.servlet.context-path"),
-                env.getProperty("spring.application.name"));
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(PortalApiApplication.class);
+        printEnvironmentInfo(app.getEnvironment());
     }
 
 }
--- shop-api/src/main/java/com/nb6868/onex/shop/ShopApiApplication.java
+++ shop-api/src/main/java/com/nb6868/onex/shop/ShopApiApplication.java
@@ -1,40 +1,24 @@
 package com.nb6868.onex.shop;
 
+import com.nb6868.onex.common.BaseApplication;
 import com.nb6868.onex.common.util.SpringBeanNameGenerator;
 import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.core.env.Environment;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
-@ComponentScan(nameGenerator = SpringBeanNameGenerator.class, basePackages = {"com.nb6868.onex.**"})
+@MapperScan(nameGenerator = SpringBeanNameGenerator.class, basePackages = {"com.nb6868.onex.**.dao", "com.nb6868.onex.shop.**.dao"})
+@ComponentScan(nameGenerator = SpringBeanNameGenerator.class, basePackages = {"com.nb6868.onex.**","com.nb6868.onex.shop.**"})
 @Slf4j
-public class ShopApiApplication extends SpringBootServletInitializer {
+public class ShopApiApplication extends BaseApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext app = SpringApplication.run(ShopApiApplication.class, args);
-
-        Environment env = app.getEnvironment();
-        log.info("\n################## {} Running ##################\n" +
-                        ":: Link ::\thttp://127.0.0.1:{}{}\n" +
-                        ":: Doc ::\thttp://127.0.0.1:{}{}/doc.html\n" +
-                        "################## {} Running ##################",
-                env.getProperty("spring.application.name"),
-                env.getProperty("server.port"),
-                env.getProperty("server.servlet.context-path"),
-                env.getProperty("server.port"),
-                env.getProperty("server.servlet.context-path"),
-                env.getProperty("spring.application.name"));
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(ShopApiApplication.class);
+        printEnvironmentInfo(app.getEnvironment());
     }
 
 }
