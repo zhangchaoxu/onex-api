dc7fb26256576c45dfe4e77a67cf19cf6e04444d
Charles <zhangchaoxu@gmail.com>
2022-02-08 10:07:08 +0800

    simplify region test

--- portal-api/src/test/java/com/nb6868/onex/api/region/RegionTest.java
+++ portal-api/src/test/java/com/nb6868/onex/api/region/RegionTest.java
@@ -1,111 +1,94 @@
 package com.nb6868.onex.api.region;
 
-import cn.afterturn.easypoi.csv.entity.CsvImportParams;
-import cn.afterturn.easypoi.csv.imports.CsvImportService;
-import cn.hutool.core.io.file.FileReader;
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.map.MapUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.http.HttpUtil;
+import cn.hutool.poi.excel.ExcelUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.nb6868.onex.api.modules.sys.dao.RegionDao;
 import com.nb6868.onex.api.modules.sys.entity.RegionEntity;
-import com.nb6868.onex.api.modules.sys.service.RegionService;
-import com.nb6868.onex.common.exception.ErrorCode;
-import com.nb6868.onex.common.util.ConvertUtils;
-import com.nb6868.onex.common.validator.AssertUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
-import java.io.IOException;
-import java.util.List;
 
 /**
  * see {https://github.com/xiangyuecn/AreaCity-JsSpider-StatsGov/}
  */
 @Slf4j
-@DisplayName("区域测试")
+@DisplayName("行政区域测试")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class RegionTest {
 
     @Resource
     private RegionDao regionDao;
-    @Autowired
-    private RegionService regionService;
 
-    /**
-     * 导入4级区域数据,【四级】省市区镇
-     */
     @Test
-    public void importDataLevel4() throws IOException {
-        String file = "c://ok_data_level4.csv";
-        CsvImportParams params = new CsvImportParams();
-        List<RegionOkDataLevel> list =  new CsvImportService().readExcel(new FileReader(file).getInputStream(), RegionOkDataLevel.class, params, null);
-        AssertUtils.isTrue(list.isEmpty(), ErrorCode.ERROR_REQUEST, "Excel内容为空");
-        for (RegionOkDataLevel item : list) {
-            RegionEntity entity = ConvertUtils.sourceToTarget(item, RegionEntity.class);
+    @DisplayName("导入4级区域数据")
+    public void importDataLevel4() {
+        String filePath = "C:\\Projects\\nb6868\\onex\\ok_data_level4.xlsx";
+        ExcelUtil.getReader(filePath).readAll().forEach(map -> {
+            RegionEntity entity = new RegionEntity();
+            entity.setId(MapUtil.getLong(map, "id"));
+            entity.setPid(MapUtil.getLong(map, "pid"));
+            entity.setDeep(MapUtil.getInt(map, "deep"));
+            entity.setName(MapUtil.getStr(map, "name"));
+            entity.setPinyinPrefix(MapUtil.getStr(map, "pinyin_prefix"));
+            entity.setPinyin(MapUtil.getStr(map, "pinyin"));
+            entity.setExtName(MapUtil.getStr(map, "ext_name"));
             regionDao.insert(entity);
-        }
+        });
         log.info("4级区域数据-导入完成");
     }
 
     /**
      * 导入3级区域中心点和边界
      */
-    /*@Test
-    public void importGeo() throws IOException {
-        // 先清空数据
-        regionDao.update(new RegionEntity(), new UpdateWrapper<RegionEntity>().set("geo", null).set("polyline", null));
-        File file = new File("c://ok_geo.csv");
-        CsvImportParams params = new CsvImportParams();
-        List<RegionOkGeo> list =  new CsvImportService().readExcel(FileUtils.openInputStream(file), RegionOkGeo.class, params, null);
-        AssertUtils.isTrue(list.isEmpty(), ErrorCode.ERROR_REQUEST, "Excel内容为空");
-        for (RegionOkGeo item : list) {
-            //regionDao.update(new RegionEntity(), new UpdateWrapper<RegionEntity>().eq("id", item.getId()).set("geo", item.getGeo()).set("polyline", item.getPolygon()));
-        }
-        log.info("3级区域数据边界-更新完成");
-    }*/
-
-    /**
-     * 导入3级区域中心点和边界
-     */
     @Test
+    @DisplayName("导入3级区域中心点和边界")
     public void importGeo() {
-        // 先清空数据
-        //regionDao.update(new RegionEntity(), new UpdateWrapper<RegionEntity>().set("geo", null).set("polygon", null));
-        /*String sql = "select * from ok_geo";
-        List<Map<String, Object>> list = dbUtils.executeQuerySql(sql);
-        log.info("list size = " + list.size());
-        list.forEach(item -> {
-            log.info(item.toString());
-            regionService.update().set("geo", item.get("geo").toString().trim().replaceAll(" ", ","))
-                    .set("polygon", item.get("polygon").toString().trim().replaceAll(",", "_").replaceAll(" ", ","))
-                    .eq("id", item.get("id")).update();
+        String filePath = "C:\\Projects\\nb6868\\onex\\ok_geo.xlsx";
+        ExcelUtil.getReader(filePath).readAll().forEach(map -> {
+            // 判断
+            String geo = MapUtil.getStr(map, "geo");
+            String polygon = MapUtil.getStr(map, "polygon");
+            if (StrUtil.isNotBlank(geo)) {
+                regionDao.update(new RegionEntity(), new UpdateWrapper<RegionEntity>()
+                        .eq("id", MapUtil.getLong(map, "id"))
+                        .set("geo", geo.replace(" ", ","))
+                        .set("polygon", polygon));
+            }
         });
-        regionService.update().set("geo", null).eq("geo", "EMPTY").update();
-        regionService.update().set("polygon", null).eq("polygon", "EMPTY").update();
-        log.info("3级区域数据边界-更新完成");*/
+        log.info("中心点和边界-导入完成");
     }
 
     /**
-     * 站点地址：http://datav.aliyun.com/tools/atlas/index.html
+     * 站点地址
+     * http://datav.aliyun.com/tools/atlas/index.html
+     * https://geo.datav.aliyun.com/areas_v3/bound/100000_full.json
      */
     @Test
     @DisplayName("下载json文件")
     public void downloadRegionJson() {
-        // 先清空数据
-        //regionDao.update(new RegionEntity(), new UpdateWrapper<RegionEntity>().set("geo", null).set("polygon", null));
-        /*String sql = "select * from ok_geo";
-        List<Map<String, Object>> list = dbUtils.executeQuerySql(sql);
-        log.info("list size = " + list.size());
-        list.forEach(item -> {
-            log.info(item.toString());
-            regionService.update().set("geo", item.get("geo").toString().trim().replaceAll(" ", ","))
-                    .set("polygon", item.get("polygon").toString().trim().replaceAll(",", "_").replaceAll(" ", ","))
-                    .eq("id", item.get("id")).update();
+        regionDao.selectList(new QueryWrapper<RegionEntity>().lt("deep", 3)).forEach(regionEntity -> {
+            String fileUrl = "https://geo.datav.aliyun.com/areas_v3/bound/";
+            if (regionEntity.getDeep() < 3) {
+                fileUrl += (StrUtil.fillAfter(regionEntity.getId().toString(), '0', 6) + "_full");
+            } else {
+                fileUrl += (StrUtil.fillAfter(regionEntity.getId().toString(), '0', 6));
+            }
+            fileUrl += ".json";
+            log.error("download json={}", fileUrl);
+            try {
+                HttpUtil.downloadFile(fileUrl, FileUtil.file("C:\\Projects\\nb6868\\onex\\geo_json\\" + regionEntity.getId() + ".json"));
+            } catch (Exception e) {
+                log.error("download json error={}", fileUrl, e);
+            }
         });
-        regionService.update().set("geo", null).eq("geo", "EMPTY").update();
-        regionService.update().set("polygon", null).eq("polygon", "EMPTY").update();
-        log.info("3级区域数据边界-更新完成");*/
     }
 
 }
