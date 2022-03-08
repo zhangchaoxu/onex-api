52962c16a6f4fed8c23ad53b4571109efa78ed82
Charles <zhangchaoxu@gmail.com>
2022-03-08 17:31:24 +0800

    upgrade libs to 3.0.0

--- portal-api/src/main/java/com/nb6868/onex/api/modules/sys/dto/LogDTO.java
+++ portal-api/src/main/java/com/nb6868/onex/api/modules/sys/dto/LogDTO.java
@@ -1,5 +1,6 @@
 package com.nb6868.onex.api.modules.sys.dto;
 
+import cn.hutool.json.JSONObject;
 import com.nb6868.onex.common.pojo.BaseDTO;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -21,23 +22,14 @@ public class LogDTO extends BaseDTO {
     @ApiModelProperty(value = "用户操作")
     private String operation;
 
-    @ApiModelProperty(value = "请求URI")
-    private String uri;
-
-    @ApiModelProperty(value = "请求方式")
-    private String method;
+    @ApiModelProperty(value = "请求参数")
+    private JSONObject requestParams;
 
     @ApiModelProperty(value = "请求参数")
     private String params;
 
     @ApiModelProperty(value = "耗时(毫秒)")
     private Long requestTime;
-
-    @ApiModelProperty(value = "用户代理")
-    private String userAgent;
-
-    @ApiModelProperty(value = "操作IP")
-    private String ip;
 
     @ApiModelProperty(value = "状态  0：失败   1：成功")
     private Integer state;
