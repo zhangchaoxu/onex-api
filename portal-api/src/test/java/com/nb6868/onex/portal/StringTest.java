8fb282b7c8af39cabe6884c0873cf4c749a72704
Charles <zhangchaoxu@gmail.com>
2022-04-28 16:59:10 +0800

    encode password

--- portal-api/src/test/java/com/nb6868/onex/portal/StringTest.java
+++ portal-api/src/test/java/com/nb6868/onex/portal/StringTest.java
@@ -1,5 +1,6 @@
 package com.nb6868.onex.portal;
 
+import com.baomidou.dynamic.datasource.toolkit.CryptoUtils;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -12,6 +13,15 @@ public class StringTest {
         String str = "零一二三四五六七八九0123456789!@#$%^&*()";
         System.out.println("str length:" + str.substring(0, 19) + "…");
         //System.out.println("str char length:" + str.substring(0, 20));
+
+    }
+
+    @Test
+    void resEncode() throws Exception  {
+        String password = "onex";
+        //String password = "5DziBzKnmkBfDTeC";
+        String encodePassword = CryptoUtils.encrypt(password);
+        System.out.println(encodePassword);
 
     }
 
